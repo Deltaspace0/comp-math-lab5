726781667ff663e0811c7f6d5899f6aea6c28f32
deltaspace0 <rgadeew@yandex.ru>
2023-09-08 11:03:02 +0300

    add point fields

--- src/UI.hs
+++ src/UI.hs
@@ -27,11 +27,21 @@ buildUI _ model = tree where
             , separatorLine
             , dropdown_ currentFunction functionChoices et et
                 [onChange AppFunctionChange]
-            ]
+            , separatorLine
+            , vstack'
+                [ label "Add points with right mouse button"
+                , vscroll $ vstack' $
+                    [ hgrid'
+                        [ label "X:"
+                        , label "Y:"
+                        ]
+                    ] <> pointPanels
+                ]
+            ] `styleBasic` [sizeReqW $ fixedSize 320]
         ] `styleBasic` [padding 16]
     points =
         [
-            [ graphPoints $ model ^. dataPoints
+            [ graphPoints ps
             , graphColor black
             , graphSeparate
             , graphOnChange AppPointChange
@@ -43,10 +53,27 @@ buildUI _ model = tree where
                 , graphColor brown
                 ]
         ]
+    pointPanels = makePointPanel <$> [0..length ps-1]
+    makePointPanel i = hgrid'
+        [ numericField_ (pointField i . _1)
+            [ decimals 3
+            , onChange $ \x -> AppPointChange i (x, psy!!i)
+            ]
+        , numericField_ (pointField i . _2)
+            [ decimals 3
+            , readOnly_ $ not $ null $ model ^. currentFunction
+            ]
+        ]
+    pointField i = lens getter setter where
+        getter = (^?! ix i) . _amDataPoints
+        setter = flip $ set $ dataPoints . ix i
     functionChoices = Nothing:(pure <$> [0..length functions-1])
     cf = fst $ functions!!(fromJust $ model ^. currentFunction)
     et i = label $ if null i
         then "No function"
         else snd $ functions!!(fromJust i)
     xs = [-15, (-14.95)..15]
+    ps = model ^. dataPoints
+    (_, psy) = unzip ps
     vstack' = vstack_ [childSpacing_ 16]
+    hgrid' = hgrid_ [childSpacing_ 16]
